a1c552dc01d5bd3a76c06301374c39714352ef5d
Andrew Disley <andrew@simplified.co.uk>
2022-08-12 16:44:21 +0100

    Adds html-minifier

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,32 @@
+const htmlmin = require('html-minifier');
+
 module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy({ 'src/static/': '/' });
 
+  eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
+    if (outputPath && outputPath.endsWith('.html')) {
+      let minified = htmlmin.minify(content, {
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true,
+        processScripts: ['text/javascript'],
+        useShortDoctype: true,
+        removeComments: true,
+      });
+      return minified;
+    }
+
+    return content;
+  });
+
   return {
     dir: {
+      data: '_data',
+      includes: '_includes',
       input: 'src',
-      output: 'dist'
-    }
-  }
+      output: 'dist',
+    },
+    htmlTemplateEngine: 'njk',
+  };
 };
